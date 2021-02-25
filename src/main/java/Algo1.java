20ee1b5fd079bad0a93f8cde8185b25ab7109d65
Audreyhrtt <36987153+Audreyhrtt@users.noreply.github.com>
2021-02-25 21:16:15 +0100

    Update Algo1.java

--- src/main/java/Algo1.java
+++ src/main/java/Algo1.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Algo1 {
 
@@ -7,9 +10,18 @@ public class Algo1 {
     }
 
     public List<Schedule> run(DataSet dataSet) {
-    	List<Schedule> list = new ArrayList<Schedule>();
+    	List<Schedule> schedules = new ArrayList<Schedule>();
+    	
+    	List<Voiture> voitures = dataSet.getListeVoitures();
+    	Map<String, Rue> rues = dataSet.getListeRues();
+    	
+    	Set<Rue> ruesUtilisees = new HashSet<Rue>();
+    	for (Voiture voiture : voitures) {
+    		for (Rue rue : voiture.getRues()) {
+    			ruesUtilisees.add(rue);
+    		}
+    	}
 
-
-    	return list;
+    	return schedules;
     }
 }
